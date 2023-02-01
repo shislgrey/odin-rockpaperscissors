97595eae927db2c39109c8964f7510b2da52e27a
Pavel Kirkovsky <pavel@kirkovsky.com>
2023-01-31 22:48:43 -0800

    Added more tests

--- rockpaperscissors.test.js
+++ rockpaperscissors.test.js
@@ -4,3 +4,13 @@ const RockPaperScissors = require('./rockpaperscissors');
 test('Check the computer choice is valid', () => {
   expect(RockPaperScissors.choices).toContain(RockPaperScissors.getComputerChoice());
 });
+
+test('Verify player choice is normalized', () => {
+  // getPlayerChoice() processes the user's input. How do we test it?
+  fail('TODO: write test code')
+});
+
+test('Verify winner selection', () => {
+  // Test the different branches of determineWinner
+  fail('TODO: write test code')
+});
