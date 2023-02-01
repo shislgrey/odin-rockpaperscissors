6f0c026cd1ad173c7237eade67428ab7505e50cc
Pavel Kirkovsky <pavel@kirkovsky.com>
2023-01-31 21:33:14 -0800

    Changed tests a bit

--- rockpaperscissors.test.js
+++ rockpaperscissors.test.js
@@ -1,5 +1,8 @@
 const RockPaperScissors = require('./rockpaperscissors');
 
-test('computer selects a valid choice', () => {
-  expect(RockPaperScissors.getComputerChoice().includes(['rock', 'paper', 'scissors'])).toBeTruthy;
+
+// TODO: fix this failing test
+// TODO: use an exported variable here, maybe
+test('Check the computer choice is valid', () => {
+  expect(['rock', 'papier', 'scissors']).toContain(RockPaperScissors.getComputerChoice());
 });
