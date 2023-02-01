291d48b5f4ef5bb7762a5f07e0781b13a4acfebf
Pavel Kirkovsky <pavel@kirkovsky.com>
2023-01-31 23:22:59 -0800

    More tests

--- rockpaperscissors.test.js
+++ rockpaperscissors.test.js
@@ -2,35 +2,51 @@ const RockPaperScissors = require('./rockpaperscissors');
 
 
 describe("getComputerChoice", () => {
-  test('it should return a valid choice', () => {
+  it('should return a valid choice', () => {
     expect(RockPaperScissors.choices).toContain(RockPaperScissors.getComputerChoice());
   });
-  test('it should return one choice', () => {
+  it('should return one choice', () => {
     throw Error('TODO: write test code');
   });
-  test('returned choice should be lowercase', () => {
+  it('should return choice in lowercase', () => {
     throw Error('TODO: write test code');
   });
 })
 
 
 describe("getPlayerChoice", () => {
-  test("it should reject invalid choices", () => {
+  it("should reject invalid choice", () => {
     throw Error('TODO: write test code');
   });
-  test("it should re-prompt on invalid choice", () => {
+  it("should re-prompt on invalid choice", () => {
     throw Error('TODO: write test code');
   });
 })
 
 describe("determineWinner", () => {
-  test('it should determine if you lost', () => {
+  it('should determine if you lost', () => {
     throw Error('TODO: write test code');
   });
-  test('it should determine if you won', () => {
+  it('should determine if you won', () => {
     throw Error('TODO: write test code');
   });
-  test('it should determine if the round is a tie', () => {
+  it('should determine if the round is a tie', () => {
+    throw Error('TODO: write test code');
+  });
+})
+
+// Move the scoring part of playGame() into its own function
+describe("scoreGame", () => {
+  it('should determine if you won', () => {
+    throw Error('TODO: write test code');
+  });
+  it('should determine if computer won', () => {
+    throw Error('TODO: write test code');
+  });
+  it('should determine if the game is a tie', () => {
+    throw Error('TODO: write test code');
+  });
+  it('should output the final score', () => {
     throw Error('TODO: write test code');
   });
 })
