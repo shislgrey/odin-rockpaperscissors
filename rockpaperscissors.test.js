15d2cda56bf2fc2fc8b9f96680b70f71a705ecb5
Grace <shisler.grace@gmail.com>
2023-01-31 21:49:21 -0800

    fixed test and adjusted test criteria

--- rockpaperscissors.test.js
+++ rockpaperscissors.test.js
@@ -1,8 +1,6 @@
 const RockPaperScissors = require('./rockpaperscissors');
 
 
-// TODO: fix this failing test
-// TODO: use an exported variable here, maybe
 test('Check the computer choice is valid', () => {
-  expect(['rock', 'papier', 'scissors']).toContain(RockPaperScissors.getComputerChoice());
+  expect(RockPaperScissors.choices).toContain(RockPaperScissors.getComputerChoice());
 });
