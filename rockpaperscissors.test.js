6de6fdaa68abcb97bbefa140cc5e07f5d171fd40
Grace <shisler.grace@gmail.com>
2023-02-06 00:50:47 -0800

    formatting
    Co-authored-by: Pavel <pavel@kirkovsky.com>

--- rockpaperscissors.test.js
+++ rockpaperscissors.test.js
@@ -17,7 +17,7 @@ describe("getComputerChoice", () => {
   it('should be fairly random', () => {
     // This will take some extra work.
     // Hint: collect a bunch of computer choices, then process them
-    throw Error('TODO: write test code');
+    throw newError('TODO: write test code');
   });
 })
 
@@ -25,40 +25,41 @@ describe("getComputerChoice", () => {
 describe("getPlayerChoice", () => {
   // TODO: Rewrite getPlayerChoice and decouple it from the prompting
   it("should accept valid choices", () => {
-    //expect(RockPaperScissors.choices).toContain(RockPaperScissors.getPlayerChoice()); **WORK ON REWRITING THIS TOMORROW
+    //TODO: WORK ON REWRITING THIS TOMORROW
+    expect(RockPaperScissors.choices).toContain(RockPaperScissors.getPlayerChoice());
   });
   it("should reject invalid choices", () => {
-    throw Error('TODO: write test code');
+    throw new Error('TODO: write test code');
   });
   it("should re-prompt on invalid choice", () => {
-    throw Error('TODO: write test code');
+    throw new Error('TODO: write test code');
   });
 })
 
 describe("determineWinner", () => {
   it('should determine if you lost', () => {
-    throw Error('TODO: write test code');
+    throw new Error('TODO: write test code');
   });
   it('should determine if you won', () => {
-    throw Error('TODO: write test code');
+    throw new Error('TODO: write test code');
   });
   it('should determine if the round is a tie', () => {
-    throw Error('TODO: write test code');
+    throw new Error('TODO: write test code');
   });
 })
 
 // Move the scoring part of playGame() into its own function
 describe("scoreGame", () => {
   it('should determine if you won', () => {
-    throw Error('TODO: write test code');
+    throw new Error('TODO: write test code');
   });
   it('should determine if computer won', () => {
-    throw Error('TODO: write test code');
+    throw new Error('TODO: write test code');
   });
   it('should determine if the game is a tie', () => {
-    throw Error('TODO: write test code');
+    throw new Error('TODO: write test code');
   });
   it('should output the final score', () => {
-    throw Error('TODO: write test code');
+    throw new Error('TODO: write test code');
   });
 })
